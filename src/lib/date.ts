d55030236c0a90c9aeaaa7337fafb731860fd011
AndrewB92 <babujioh@gmail.com>
2026-07-02 10:24:09 +0300

    Migrate Base44 site to static Astro

--- src/lib/date.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: Date, locale = 'en-GB') {
-  return new Intl.DateTimeFormat(locale, {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric',
-  }).format(date);
-}
