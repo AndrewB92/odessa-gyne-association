750913b0a5ea879b9cba99e1c074f43ebe914a6f
AndrewB92 <babujioh@gmail.com>
2026-07-22 12:17:51 +0300

    pre-production fixes
    I guess so

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://odessa-gyne-association.pages.dev',
   output: 'static',
+  integrations: [sitemap()],
 });
